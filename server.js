01b9cceb9e5445c670066cbd5d4432cf0016acc6
NandRobo <roboresponse.ai@gmail.com>
2023-05-05 13:22:14 +0530

    customer portal and sending paramets to server and used metadata

--- server.js
+++ server.js
@@ -5,35 +5,19 @@
 const express = require('express');
 var cors = require('cors');
 const stripe = require('stripe')('sk_test_51MnPuzSDWIdOcoieFyPfq4mwrIdaIenJGKAL6YE7QHRXaoG4hgAOgYRbtpJPkH37f8BMsaDDp0ZSDYjFz7u9DzLD00tdkf7eVp');
-
+//var CUSTOMER_ID = 'cus_Noq4IzyokPrZVS'
 const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/checkout", async (req, res) => {
-    /*
-    req.body.items
-    [
-        {
-            id: 1,
-            quantity: 3
-        }
-    ]
 
-    stripe wants
-    [
-        {
-            price: 1,
-            quantity: 3
-        }
-    ]
-    */
     console.log(req.body);
     const items = req.body.items;
     console.log('items', items)
     let lineItems = [];
-    items.forEach((item)=> {
+    items.forEach((item) => {
         lineItems.push(
             {
                 price: item.id,
@@ -43,15 +27,44 @@ app.post("/checkout", async (req, res) => {
     });
 
     const session = await stripe.checkout.sessions.create({
-        line_items: lineItems,
+        //line_items: lineItems,
+        line_items: [
+            { "price": "price_1N1wjUSDWIdOcoiem4m2QHd8", "quantity": 1 },
+            //{ "price": "price_1N1yLnSDWIdOcoie463iRltL", "quantity": 1 },
+            //{ "price": "price_1N1xyNSDWIdOcoieDGXsub6U", "quantity": 1 },
+            //{ "price": "price_1N3BxTSDWIdOcoieyuJ8Rl0N", "quantity": 1 },
+        ],
         mode: 'subscription',
         success_url: "http://localhost:3000/success",
-        cancel_url: "http://localhost:3000/cancel"
+        cancel_url: "http://localhost:3000/cancel",
+        metadata: {
+            "rr_customer_id": "201",
+        },
     });
 
     res.send(JSON.stringify({
         url: session.url
     }));
+});
+
+app.post('/create-customer-portal-session', async (req, res) => {
+    // Authenticate your user.
+    console.log(req.body);
+    const CUSTOMER_ID = req.body.customer_id;
+
+    const customer = await stripe.customers.update(CUSTOMER_ID, {
+        metadata: {
+            rr_customer_id: '201',
+        },
+    });
+
+
+    const session = await stripe.billingPortal.sessions.create({
+        customer: CUSTOMER_ID,
+        return_url: 'http://localhost:3000/success',
+    });
+
+    res.redirect(session.url);
 });
 
 app.listen(4000, () => console.log("Listening on port 4000!"));
--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import {Button, Container, Navbar, Modal} from 'react-bootstrap';
+import { Button, Container, Navbar, Modal } from 'react-bootstrap';
 import { useState, useContext } from 'react';
 import { CartContext } from "../CartContext";
 import CartProduct from './CartProduct';
@@ -16,11 +16,29 @@ function NavbarComponent() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({items: cart.items})
+            body: JSON.stringify({ items: cart.items })
         }).then((response) => {
             return response.json();
         }).then((response) => {
-            if(response.url) {
+            if (response.url) {
+                window.location.assign(response.url); // Forwarding user to Stripe
+            }
+        });
+    }
+
+    const manageBilling = async () => {
+        await fetch('http://localhost:4000/create-customer-portal-session', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ customer_id: "cus_NpOdr1RzvHVS11" })
+        }).then((response) => {
+            console.log(response)
+            return response.json();
+        }).then((response) => {
+            if (response.url) {
+                console.log(response.url)
                 window.location.assign(response.url); // Forwarding user to Stripe
             }
         });
@@ -45,7 +63,7 @@ function NavbarComponent() {
                     {productsCount > 0 ?
                         <>
                             <p>Items in your cart:</p>
-                            {cart.items.map( (currentProduct, idx) => (
+                            {cart.items.map((currentProduct, idx) => (
                                 <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
                             ))}
 
@@ -54,8 +72,14 @@ function NavbarComponent() {
                             <Button variant="success" onClick={checkout}>
                                 Purchase items!
                             </Button>
+                            <Button variant="success" onClick={manageBilling}>
+                                Manage billing
+                            </Button>
+                            {/*<form method="POST" action="/create-customer-portal-session">
+                                <button type="submit">Manage billing</button>
+                            </form>*/}
                         </>
-                    :
+                        :
                         <h1>There are no items in your cart!</h1>
                     }
                 </Modal.Body>
